4e28d4675baecd4ac19cc6b5bd99975ed2ce6ba1
Jon Battista <jonbattista@gmail.com>
2021-04-01 19:58:05 -0400

    wip

--- server.py
+++ server.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 @app.route('/', methods=["POST"])
 
 def alpaca():
-	key_id = request.args.get('key_id')
+    key_id = request.args.get('key_id')
 
-	secret_key = request.args.get('secret_key')
+    secret_key = request.args.get('secret_key')
 
-	if key_id is not None and secret_key is not None:
-		headers = Headers()
+    if key_id is not None and secret_key is not None:
+        headers = Headers()
 
-		url = 'https://api.alpaca.markets/v2/orders'
+        url = 'https://api.alpaca.markets/v2/orders'
 
-	    headers.add('APCA_API_SECRET_KEY', key_id)
-	    headers.add('APCA_API_SECRET_KEY', secret_key)
+        headers.add('APCA_API_SECRET_KEY', key_id)
+        headers.add('APCA_API_SECRET_KEY', secret_key)
 
-	    response = requests.post(url, data=data, headers=headers)
+        response = requests.post(url, data=data, headers=headers)
 
-	    print(response)
-	
+        print(response)
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
