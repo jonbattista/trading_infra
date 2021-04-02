8a3831202b0747b736987c45aa086dada46ca9bf
Jon Battista <jonbattista@gmail.com>
2021-04-02 09:58:40 -0400

    add alpaca sdk

--- server.py
+++ server.py
@@ -1,26 +1,66 @@
-from flask import Flask
+from flask import Flask, request
 import requests
+import alpaca_trade_api as tradeapi
 
 app = Flask(__name__)
+
+app.debug = True
 
 @app.route('/', methods=["POST"])
 
 def alpaca():
-    key_id = requests.args.get('key_id')
 
-    secret_key = requests.args.get('secret_key')
 
-    if key_id is not None and secret_key is not None:
-        headers = Headers()
 
-        url = 'https://api.alpaca.markets/v2/orders'
+# Submit a market order to buy 1 share of Apple at market price
+    if request.args.get('APCA_API_KEY_ID') is None:
+        return 'APCA_API_KEY_ID is not set!', 400
 
-        headers.add('APCA_API_KEY_ID', key_id)
-        headers.add('APCA_API_SECRET_KEY', secret_key)
+    if request.args.get('APCA_API_SECRET_KEY') is None:
+        return 'APCA_API_SECRET_KEY is not set!', 400
 
-        response = requests.post(url, data=data, headers=headers)
+    if request.args.get('SYMBOL') is None:
+        return 'SYMBOL is not set!', 400
 
-        print(response)
-    
+    if request.args.get('QUANTITY') is None:
+        return 'QUANTITY is not set!', 400
+
+    if request.args.get('TYPE') is None:
+        return 'TYPE is not set!', 400
+
+    if request.args.get('ORDER_TYPE') is None:
+        return 'ORDER_TYPE is not set!', 400
+
+    if request.args.get('TIME_IN_FORCE') is None:
+        return 'TIME_IN_FORCE is not set!', 400
+
+    APCA_API_KEY_ID = request.args.get('APCA_API_KEY_ID')
+    APCA_API_SECRET_KEY = request.args.get('APCA_API_SECRET_KEY')
+    SYMBOL = request.args.get('SYMBOL')
+    QUANTITY = request.args.get('QUANTITY') 
+    TYPE = request.args.get('TYPE') 
+    ORDER_TYPE = request.args.get('ORDER_TYPE')
+    TIME_IN_FORCE = request.args.get('TIME_IN_FORCE') 
+
+    print(APCA_API_KEY_ID)
+    print(APCA_API_SECRET_KEY)
+    print(SYMBOL)
+    print(QUANTITY)
+    print(TYPE)
+    print(ORDER_TYPE)
+    print(TIME_IN_FORCE)
+
+    api = tradeapi.REST(APCA_API_KEY_ID, APCA_API_SECRET_KEY, 'https://paper-api.alpaca.markets')
+
+    account = api.get_account()
+
+    if account.trading_blocked:
+        return 'Account is currently restricted from trading.', 400
+
+    order = api.submit_order(SYMBOL, QUANTITY, TYPE, ORDER_TYPE, TIME_IN_FORCE)
+
+    print(order)
+    return f'Order was {order.status}'
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
