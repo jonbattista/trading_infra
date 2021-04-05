afb1de6c18c6881639e4a6a09e0c80b7e6553a73
Jon Battista <jonbattista@gmail.com>
2021-04-04 20:44:39 -0400

    wip

--- src/server.py
+++ src/server.py
@@ -89,43 +89,9 @@ def alpaca():
       "change_today": "0.0084"
     }
 
-    sqqq_position = {
-      "asset_id": "904837e3-3b76-47ec-b432-046db621571b",
-      "symbol": "SQQQ",
-      "exchange": "NASDAQ",
-      "asset_class": "us_equity",
-      "avg_entry_price": "100.0",
-      "qty": "5",
-      "side": "long",
-      "market_value": "600.0",
-      "cost_basis": "500.0",
-      "unrealized_pl": "100.0",
-      "unrealized_plpc": "0.20",
-      "unrealized_intraday_pl": "10.0",
-      "unrealized_intraday_plpc": "0.0084",
-      "current_price": "120.0",
-      "lastday_price": "119.0",
-      "change_today": "0.0084"
-    }
+    sqqq_position = '{"asset_id": "904837e3-3b76-47ec-b432-046db621571b","symbol": "SQQQ","exchange": "NASDAQ","asset_class": "us_equity","avg_entry_price": "100.0","qty": "5","side": "long","market_value": "600.0","cost_basis": "500.0","unrealized_pl": "100.0","unrealized_plpc": "0.20","unrealized_intraday_pl": "10.0","unrealized_intraday_plpc": "0.0084","current_price": "120.0","lastday_price": "119.0","change_today": "0.0084"}'
 
-    tqqq_position = {
-      "asset_id": "904837e3-3b76-47ec-b432-046db621571a",
-      "symbol": "TQQQ",
-      "exchange": "NASDAQ",
-      "asset_class": "us_equity",
-      "avg_entry_price": "100.0",
-      "qty": "5",
-      "side": "long",
-      "market_value": "600.0",
-      "cost_basis": "500.0",
-      "unrealized_pl": "100.0",
-      "unrealized_plpc": "0.20",
-      "unrealized_intraday_pl": "10.0",
-      "unrealized_intraday_plpc": "0.0084",
-      "current_price": "120.0",
-      "lastday_price": "119.0",
-      "change_today": "0.0084"
-    }
+    tqqq_position = '{"asset_id": "904837e3-3b76-47ec-b432-046db621571a","symbol": "TQQQ","exchange": "NASDAQ","asset_class": "us_equity","avg_entry_price": "100.0","qty": "5","side": "long","market_value": "600.0","cost_basis": "500.0","unrealized_pl": "100.0","unrealized_plpc": "0.20","unrealized_intraday_pl": "10.0","unrealized_intraday_plpc": "0.0084","current_price": "120.0","lastday_price": "119.0","change_today": "0.0084"}'
     print(json.loads(tqqq_position))
     print(json.loads(sqqq_position))
     print(portfolio)
