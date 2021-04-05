4c600833f9ac26bf549c69fc24d29898728a9648
Jon Battista <jonbattista@gmail.com>
2021-04-04 22:16:17 -0400

    wip

--- src/server.py
+++ src/server.py
@@ -1,3 +1,35 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@jonbattista 
+jonbattista
+/
+trading_infra
+1
+0
+0
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+trading_infra/src/server.py /
+@jonbattista
+jonbattista wip
+Latest commit 05df577 20 minutes ago
+ History
+ 1 contributor
+230 lines (195 sloc)  8.46 KB
+  
 from flask import Flask, request
 import requests
 import os
@@ -66,165 +98,38 @@ def alpaca():
         return 'Account is currently restricted from trading.', 400
     open_orders = api.list_orders()
     if not open_orders:
-        print('Not Open Orders found')
-        print(f'{len(open_orders)} Open Orders found!')
+        print('No Open Orders found')
     else:
         print(open_orders)
+        print(f'{len(open_orders)} Open Orders found!')
 
-    #portfolio = api.list_positions()
-    portfolio = {
-      "asset_id": "904837e3-3b76-47ec-b432-046db621571b",
-      "symbol": "AAPL",
-      "exchange": "NASDAQ",
-      "asset_class": "us_equity",
-      "avg_entry_price": "100.0",
-      "qty": "5",
-      "side": "long",
-      "market_value": "600.0",
-      "cost_basis": "500.0",
-      "unrealized_pl": "100.0",
-      "unrealized_plpc": "0.20",
-      "unrealized_intraday_pl": "10.0",
-      "unrealized_intraday_plpc": "0.0084",
-      "current_price": "120.0",
-      "lastday_price": "119.0",
-      "change_today": "0.0084"
-    }
 
-    sqqq_position = json.loads('{"asset_id": "904837e3-3b76-47ec-b432-046db621571b","symbol": "SQQQ","exchange": "NASDAQ","asset_class": "us_equity","avg_entry_price": "100.0","qty": "5","side": "long","market_value": "600.0","cost_basis": "500.0","unrealized_pl": "100.0","unrealized_plpc": "0.20","unrealized_intraday_pl": "10.0","unrealized_intraday_plpc": "0.0084","current_price": "120.0","lastday_price": "119.0","change_today": "0.0084"}')
-
-    tqqq_position = json.loads('{"asset_id": "904837e3-3b76-47ec-b432-046db621571a","symbol": "TQQQ","exchange": "NASDAQ","asset_class": "us_equity","avg_entry_price": "100.0","qty": "5","side": "long","market_value": "600.0","cost_basis": "500.0","unrealized_pl": "100.0","unrealized_plpc": "0.20","unrealized_intraday_pl": "10.0","unrealized_intraday_plpc": "0.0084","current_price": "120.0","lastday_price": "119.0","change_today": "0.0084"}')
-    print(sqqq_position) 
-    print(tqqq_position)
-    print(portfolio)
-
-    # Recieved a Buy TQQQ alert
-    if side == 'buy':
-        if not portfolio:
-            print('No Open positions found!')
-        else:
-            # Check if there is an open SQQQ position
-            #try:   
-            #    sqqq_position = api.get_position('SQQQ')
-            #except tradeapi.rest.APIError as exception:
-            #    print(exception)
+    buying_power = Decimal(account.buying_power)
             
-            # Sell SQQQ positions at Market
-            try:
-                order = api.submit_order(
-                    symbol='SQQQ',
-                    qty=sqqq_position['qty'],
-                    side='sell',
-                    type='market',
-                    time_in_force=time_in_force,
-                )
-            except tradeapi.rest.APIError as exception:
-                print(exception)
+    print(f'Buying Power is {buying_power}')
+            
+    limit_price = Decimal(price) * Decimal('.0.5')
 
+    if buying_power > 0:
+        number_of_shares = round(buying_power // limit_price)
+        if number_of_shares > 0:
+            order = api.submit_order(
+                symbol=ticker,
+                qty=number_of_shares - 1,
+                side=side,
+                type=order_type,
+                time_in_force=time_in_force,
+                limit_price=limit_price
+            )
+            print(order)
             if order.status == 'accepted':
-                result = f'Success: Sale of {order.filled_qty} of TQQQ was {order.status}'
-
-                message = Mail(
-                    from_email='alerts@trading.battista.dev',
-                    to_emails='jonbattista@gmail.com',
-                    subject=f'{result}')
-                try:
-                    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-                    response = sg.send(message)
-                    print(response.status_code)
-                    print(response.body)
-                    print(response.headers)
-                except Exception as e:
-                    print(e)
-
-                return result
+                return f'Success: Purchase of {number_of_shares} at ${price} was {order.status}'
             else:
-                result = f'Error: Sale of TQQQ was {order.status}'
-                message = Mail(
-                    from_email='alerts@trading.battista.dev',
-                    to_emails='jonbattista@gmail.com',
-                    subject=f'{result}')
-                try:
-                    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-                    response = sg.send(message)
-                    print(response.status_code)
-                    print(response.body)
-                    print(response.headers)
-                except Exception as e:
-                    print(e.message)
-                return result
-
-    # Recieved a Sell TQQQ alert
-    elif side == 'sell':
-        if not portfolio:
-            print('No Open positions found!')
+                return f'Error: Purchase of {number_of_shares} at ${price} was {order.status}'
         else:
-            # Check if there is an open TQQQ position
-            #try:
-            #    tqqq_position = api.get_position('TQQQ')
-            #except tradeapi.rest.APIError as exception:
-            #    print(exception)
-
-            # Sell TQQQ positions at Market price
-            try:
-                order = api.submit_order(
-                    symbol='TQQQ',
-                    qty=tqqq_position['qty'],
-                    side='sell',
-                    type='market',
-                    time_in_force=time_in_force,
-                )
-            except tradeapi.rest.APIError as exception:
-                print(exception)
-
-            if order.status == 'accepted':
-                return f'Success: Sale of {tqqq_position.filled_qty} of TQQQ was {order.status}'
-            else:
-                return f'Error: Sale of {tqqq_position.filled_qty} of TQQQ was {order.status}'
-
-            # Wait for TQQQ Sell Order to be filled
-            tqqq_order_id = order.asset_id
-
-            tqqq_order_filled = False
-
-            while not tqqq_order_filled:
-                tqqq_order_status = get_order_by_client_order_id(tqqq_order_id)
-                if tqqq_order_status == 'filled':
-                    tqqq_order_filled = True
-
-            # Check if there is an TQQQ position is closed
-            try:
-                tqqq_position = api.get_position('TQQQ')
-            except tradeapi.rest.APIError as exception:
-                print(exception)
-
-            buying_power = Decimal(account.buying_power)
-            
-            print(f'Buying Power is {buying_power}')
-            
-            limit_price = Decimal(price) * Decimal('.0.5')
-
-            if buying_power != 0:
-                number_of_shares = round(buying_power // price)
-                if number_of_shares > 0:
-                    order = api.submit_order(
-                        symbol=ticker,
-                        qty=number_of_shares - 1,
-                        side=side,
-                        type=order_type,
-                        time_in_force=time_in_force,
-                        limit_price=limit_price
-                    )
-                    print(order)
-                    if order.status == 'accepted':
-                        return f'Success: Purchase of {number_of_shares} at ${price} was {order.status}'
-                    else:
-                        return f'Error: Purchase of {number_of_shares} at ${price} was {order.status}'
-                else:
-                    return f'Not enough Buying Power: ${buying_power}', 200
-            
-            return f'You have no Buying Power: ${buying_power}', 200
-    return 'hello', 200
+            return f'Error: Not enough Buying Power: ${buying_power} to buy {number_of_shares} at limit price {limit_price}', 200
+    
+    return f'Error: You have no Buying Power: ${buying_power}', 200
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
