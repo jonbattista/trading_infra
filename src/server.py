5ce39c5bd114d994dedde720e8cf91c4b8a7fc21
Jon Battista <jonbattista@gmail.com>
2021-04-04 21:46:15 -0400

    wip

--- src/server.py
+++ src/server.py
@@ -93,6 +93,8 @@ def alpaca():
     sqqq_position = json.loads('{"asset_id": "904837e3-3b76-47ec-b432-046db621571b","symbol": "SQQQ","exchange": "NASDAQ","asset_class": "us_equity","avg_entry_price": "100.0","qty": "5","side": "long","market_value": "600.0","cost_basis": "500.0","unrealized_pl": "100.0","unrealized_plpc": "0.20","unrealized_intraday_pl": "10.0","unrealized_intraday_plpc": "0.0084","current_price": "120.0","lastday_price": "119.0","change_today": "0.0084"}')
 
     tqqq_position = json.loads('{"asset_id": "904837e3-3b76-47ec-b432-046db621571a","symbol": "TQQQ","exchange": "NASDAQ","asset_class": "us_equity","avg_entry_price": "100.0","qty": "5","side": "long","market_value": "600.0","cost_basis": "500.0","unrealized_pl": "100.0","unrealized_plpc": "0.20","unrealized_intraday_pl": "10.0","unrealized_intraday_plpc": "0.0084","current_price": "120.0","lastday_price": "119.0","change_today": "0.0084"}')
+    print(sqqq_position) 
+    print(tqqq_position)
     print(portfolio)
 
     # Recieved a Buy TQQQ alert
